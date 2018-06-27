0e777619a21f5934a339a64dd26dbce67566bb58
Saleh Masum <salehmasum@gmail.com>
2018-06-27 19:08:01 +1000

    Initial Commit

---
+++ DynamicTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DynamicTable
+//
+//  Created by user on 27/6/18.
+//  Copyright © 2018 SM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
